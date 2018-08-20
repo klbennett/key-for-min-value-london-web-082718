bd3709deca49f771005fefb51d3a445dd8c501f4
Kay B <kaylouisebennett@gmail.com>
2018-08-20 22:16:38 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,15 +3,13 @@
 
 
 def key_for_min_value(name_hash)
-  if name_hash.length == 0
-    nil
-  else
   smallest_value = nil
   key_smallest_value = ""
   name_hash.each do |name, value| 
-    if smallest_value == nil || value < smallest_value
-      smallest_value = value
+    if smallest_value == nil || value < 
+      smallest_value
       key_smallest_value = name
+      smallest_value = value
     end
 name_with_smallest_value
   end
